d10eed58087d04e90ee45039f7c7e50eb8006f07
Bharath varma <99080776+Moderli@users.noreply.github.com>
2025-07-19 17:21:31 +0530

    404 to /
    updated the code so that if a person came across an error then send him to the main page or index page. it can be 404 or anything equilent

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import Game from "./pages/Game";
 import { useEffect } from 'react';
 import { startTimeTracking, updateTimeSpent } from '@/lib/metrics';
@@ -43,7 +42,7 @@ const App = () => {
             <Route path="/" element={<Index />} />
             <Route path="/game" element={<Game />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
         <Analytics />
